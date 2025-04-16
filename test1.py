39134190a8820cd8dbbcf17d6716eba257cdc8e5
u5710587 <u5710587@gmail.com>
2025-04-16 16:06:49 +0800

    第二次修改test1.py

--- test1.py
+++ test1.py
@@ -8,5 +8,5 @@ Created on Wed Apr 16 15:37:02 2025
 num=eval(input("請輸入九九乘法表最大顯示的數值:"))
 for i in range(1,num+1):
 	for j in range(1,num+1):
-		print("%d*%d=%d" %(i,j,i*j),end=" ")
+		print("%d*%d=%d" %(i,j,i*j),end="\t")
 	print()
